4ad7cfdc7d8ce01a99ce8260e82ac18507d66899
AliNisarAhmed <ali.nisar87@gmail.com>
2019-05-11 12:41:44 -0600

    applied nav styles, added nProgress bar, server rendered css needs fixing

--- components/Page.js
+++ components/Page.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import Header from './Header';
-import Meta from './Meta';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import React, { Component } from "react";
+import Header from "./Header";
+import Meta from "./Meta";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 
 const theme = {
-  red: '#FF0000',
-  black: '#393939',
-  grey: '#3A3A3A',
-  lightgrey: '#E1E1E1',
-  offwhite: '#EDEDED',
-  maxWidth: '1000px',
-  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offwhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 };
 
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
@@ -25,6 +25,35 @@ const Inner = styled.div`
   background: ${props => props.theme.red};
 `;
 
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2');
+    font-weight: normal;
+    font-style: normal; 
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 export default class Page extends Component {
   render() {
     return (
@@ -32,11 +61,9 @@ export default class Page extends Component {
         <StyledPage>
           <Meta />
           <Header />
-          <Inner>
-            {this.props.children}
-          </Inner>
+          <Inner>{this.props.children}</Inner>
         </StyledPage>
       </ThemeProvider>
-    )
+    );
   }
 }
